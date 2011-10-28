24f59fd294d1564aa5c229c10c43168c6e1e2cbe
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2011-10-28 21:11:59 +0200

    Nuevo script para facilitar la creación del instalador para Windows

---
+++ redistribuir.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp alien.blb /media/sda1/Documents\ and\ Settings/Ricardo/Mis\ documentos/Redistribuir\ Gargoyle/Empaquetar/Instalador\ EE/
+cp garglk.ini /media/sda1/Documents\ and\ Settings/Ricardo/Mis\ documentos/Redistribuir\ Gargoyle/Empaquetar/Instalador\ EE/
+cp doc/Instrucciones_Alien.pdf /media/sda1/Documents\ and\ Settings/Ricardo/Mis\ documentos/Redistribuir\ Gargoyle/Empaquetar/Instalador\ EE/instrucciones.pdf
+
