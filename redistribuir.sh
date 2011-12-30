311d4ef0061bf98a91664417b852dcb9afabe644
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2011-12-30 11:46:53 +0100

    Actualiza archivo redistribuir.sh

--- redistribuir.sh
+++ redistribuir.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
+# Para Windows:
 cp alien.blb /media/sda1/Documents\ and\ Settings/Ricardo/Mis\ documentos/Redistribuir\ Gargoyle/Empaquetar/Instalador\ EE/
 cp garglk.ini /media/sda1/Documents\ and\ Settings/Ricardo/Mis\ documentos/Redistribuir\ Gargoyle/Empaquetar/Instalador\ EE/
 cp Redist/* /media/sda1/Documents\ and\ Settings/Ricardo/Mis\ documentos/Redistribuir\ Gargoyle/Empaquetar/Instalador\ EE/
+
+# Para Linux:
+cp Redist/cover.jpg /home/ricardo/IF/Empaquetar/Alien_EE/alphaaventuras-alien_081124/usr/share/doc/alphaaventuras-alien/
+cp Redist/holocubo.pdf /home/ricardo/IF/Empaquetar/Alien_EE/alphaaventuras-alien_081124/usr/share/doc/alphaaventuras-alien/
+cp Redist/instrucciones.pdf /home/ricardo/IF/Empaquetar/Alien_EE/alphaaventuras-alien_081124/usr/share/doc/alphaaventuras-alien/
+cp Redist/plan_ejecucion.pdf /home/ricardo/IF/Empaquetar/Alien_EE/alphaaventuras-alien_081124/usr/share/doc/alphaaventuras-alien/
+cp alien.blb /home/ricardo/IF/Empaquetar/Alien_EE/alphaaventuras-alien_081124/usr/share/games/alphaaventuras-alien/
+cp garglk.ini /home/ricardo/IF/Empaquetar/Alien_EE/alphaaventuras-alien_081124/usr/share/games/alphaaventuras-alien/
+
+# Multiplataforma:
+cp alien.blb /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
+cp garglk.ini /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
+cp Redist/*.url /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
+cp doc/COPYING /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
+cp Redist/copyright /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
+cp Redist/cover.jpg /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
+cp Redist/*.pdf /home/ricardo/IF/Empaquetar/Alien_EE/ZIP/Alien_EE/
 
