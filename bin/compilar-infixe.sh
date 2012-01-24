3dcd493bc8d8a927b0e7185a6f820260d70c518e
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2012-01-24 12:37:30 +0100

    Ahora, siempre que se compila, se añaden los datos bibliográficos

--- bin/compilar-infixe.sh
+++ bin/compilar-infixe.sh
@@ -6,4 +6,5 @@ cd ..
 bres alien.res alien.blc alien.bli
 inform-infixe -D -X $* \$MAX_OBJ_PROP_COUNT=100 \$MAX_LABELS=2000 +include_path=,lib,InformATE610_git,Alpha_Pack,Damusix,SGW+DMX_NG,ZIPI_NG alien.inf
 blc alien.blc alien.blb
+meta.sh
 
