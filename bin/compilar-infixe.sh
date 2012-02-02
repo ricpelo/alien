993ea05cfbd85f0b1af72e496fdc4b96777e34d5
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2012-02-02 19:22:31 +0100

    Reorganización de librerías: Damusix y SGW+DMX_NG

--- bin/compilar-infixe.sh
+++ bin/compilar-infixe.sh
@@ -4,7 +4,7 @@ cd ZIPI_NG
 ./compilar-zipi.sh
 cd ..
 bres alien.res alien.blc alien.bli
-inform-infixe -D -X $* \$MAX_OBJ_PROP_COUNT=100 \$MAX_LABELS=2000 +include_path=,lib,InformATE610_git,Alpha_Pack,Damusix,SGW+DMX_NG,ZIPI_NG alien.inf
+inform-infixe -D -X $* \$MAX_OBJ_PROP_COUNT=100 \$MAX_LABELS=2000 +include_path=,lib,InformATE610_git,Alpha_Pack,ZIPI_NG alien.inf
 blc alien.blc alien.blb
 meta.sh
 
